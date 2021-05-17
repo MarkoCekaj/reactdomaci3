c7239937d05bd3be343aec686a8edbc176871524
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-05-17 10:19:49 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Books from "./pages/books/Books";
 import Movies from "./pages/movies/Movies";
 import People from "./pages/people/People";
 import Login from "./pages/login/Login";
-
+import MoviesForm from "./pages/movies/MoviesForm";
+import BooksForm from "./pages/books/BooksForm";
+import PeopleForm from "./pages/people/PeopleForm";
 import PrivateRoute from "./privateRoute/PrivateRoute";
 function App() {
   return (
@@ -13,8 +15,11 @@ function App() {
       <Switch>
         <Route exact path="/" component={Login}></Route>
         <PrivateRoute path="/books" exact component={Books} isPrivate />
+        <PrivateRoute path="/books/:id" exact component={BooksForm} />
         <PrivateRoute path="/movies" exact component={Movies} isPrivate />
+        <PrivateRoute path="/movies/:id" exact component={MoviesForm} />
         <PrivateRoute path="/people" exact component={People} isPrivate />
+        <PrivateRoute path="/people/:id" exact component={PeopleForm} />
       </Switch>
     </div>
   );
