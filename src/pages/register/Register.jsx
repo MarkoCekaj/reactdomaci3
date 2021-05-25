d4f226ed80d12594f2a936b3a73b08468134bfee
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-05-25 15:26:55 +0200

    yup validation

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,20 +2,31 @@ import React, { useState } from 'react'
 import { useHistory } from 'react-router';
 import { authorities, register } from '../../services/account';
 import Form from 'react-bootstrap/Form'
+import { useForm } from 'react-hook-form'
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 const Register = () => {
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().required("email is required !").email("Email is invalid"),
+        login: Yup.string().required("login is required !"),
+        password: Yup.string().required("password is required").min(6, "Password must be at least 6 chars").max(15, "Password can be max 15 chars")
+    })
+    const formOptions = { resolver: yupResolver(validationSchema) }
+    const { register, handleSubmit, formState } = useForm(formOptions);
+    const { errors } = formState
+
     const history = useHistory()
     const [registerData, setRegisterData] = useState({
         email: "",
         login: "",
         password: "",
-        activated: true,
     })
     const [errorMessage, setErrorMessage] = useState("");
 
     const onSubmit = (e) => {
-
         e.preventDefault();
         register(registerData).then(function (response) {
             console.log(response);
@@ -33,11 +44,14 @@ const Register = () => {
 
     }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control
                     type="text"
+                    name="email"
+                    className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                    {...register('email')}
                     placeholder="Enter email"
                     value={registerData?.email}
                     onChange={(e) => {
@@ -49,12 +63,16 @@ const Register = () => {
                         });
                     }}
                 />
-
+                <div className="invalid-feedback">{errors.email?.message}</div>
             </Form.Group>
+
             <Form.Group controlId="formBasicUsername">
-                <Form.Label>Register</Form.Label>
+                <Form.Label>Username</Form.Label>
                 <Form.Control
+                    className={`form-control ${errors.login ? 'is-invalid' : ''}`}
                     type="text"
+                    name="login"
+                    {...register('login')}
                     placeholder="Enter login"
                     value={registerData?.login}
                     onChange={(e) => {
@@ -66,13 +84,16 @@ const Register = () => {
                         });
                     }}
                 />
-
+                <div className="invalid-feedback">{errors.login?.message}</div>
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
+                    className={`form-control ${errors.password ? 'is-invalid' : ''}`}
                     type="password"
+                    name="password"
+                    {...register('password')}
                     placeholder="Enter password"
                     value={registerData?.password}
                     onChange={(e) => {
@@ -84,8 +105,10 @@ const Register = () => {
                         });
                     }}
                 />
+                <div className="invalid-feedback">{errors.password?.message}</div>
             </Form.Group>
-            <Button onClick={(e) => onSubmit(e)} variant="primary" type="submit">
+
+            <Button variant="primary" type="submit">
                 Submit
         </Button>
             {errorMessage}
